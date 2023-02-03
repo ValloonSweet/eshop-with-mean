3367124b28b1093a27d0d808e5590f5fa071e377
ValloonSweet <valloon91105@gmail.com>
2023-02-04 05:10:50 +0800

    feat: User CRUD operation

--- BACKEND/models/users.model.ts
+++ BACKEND/models/users.model.ts
@@ -1,4 +1,20 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+export interface IUserDocument extends mongoose.Document {
+    email: string;
+    name: string;
+    passwordHash: string;
+    phone: string;
+    street: string;
+    apartment: string;
+    city: string;
+    country: string;
+    isAdmin: boolean;
+    comparePassword(candidatePassword: string): Promise<boolean>;
+    generateAuthToken(): string;
+}
 
 const userSchema = new mongoose.Schema({
     name: {type: String, required: true},
@@ -18,6 +34,37 @@ userSchema.virtual('id').get(function() {
 })
 
 userSchema.set('toJSON', {virtuals: true});
+
+userSchema.pre('save', async (next) => {
+    let user = this as unknown as IUserDocument;
+    if(!user.isModified('passwordHash'))
+        return next();
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(user.passwordHash, salt);
+    user.passwordHash = hash;
+
+    return next();
+})
+
+userSchema.methods.comparePassword = async function (candidatePassword: string) {
+    const user = this as unknown as IUserDocument;
+    return bcrypt.compare(candidatePassword, user.passwordHash)
+        .catch(e => false);
+}
+
+userSchema.methods.generateAuthToken = function () {
+    const user = this as unknown as IUserDocument;
+    const token = jwt.sign({
+        id: user.id,
+        email: user.email,
+        role: user.isAdmin
+        },
+        process.env.JWT_PRIVATE_KEY || 'secure_jwt_key',
+        {expiresIn: '24h'}
+    )
+    return token;
+}
 
 const User = mongoose.model('USER', userSchema);
 
--- BACKEND/services/user.service.ts
+++ BACKEND/services/user.service.ts
@@ -9,15 +9,15 @@ export const updateUser = async (id: string, userDto: UserDto) => {
 }
 
 export const getUsers = async () => {
-    return await User.find();
+    return await User.find().select('-passwordHash');
 }
 
 export const getUserById = async (id: string) => {
-    return await User.findById(id);
+    return await User.findById(id).select('-passwordHash');
 }
 
 export const getUserByEmail = async (email: string) => {
-    return await User.findOne({email});
+    return await User.findOne({email}).select('-passwordHash');
 }
 
 export const deleteUserById = async (id: string) => {
